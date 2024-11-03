88f5c7e1fc94b1e9b19dc6d486ec9ce3b94c0c59
tom_szpak <tszpak@brandwatch.com>
2024-11-03 00:53:59 +0000

    chore: add styling and additional image to blog click

--- src/components/Blog.tsx
+++ src/components/Blog.tsx
@@ -1,28 +1,38 @@
 import helmetLogo from '../assets/helmetjs.png'
 import debian from '../assets/debian.png'
 import cicd from '../assets/cicd.png'
+import { useState } from 'react'
 
 const Blog = () => {
+    const [clickedPost, updateClickedPost] = useState<number | null>(null)
     const blogPosts = [
         {
             id: 1,
             title: 'Add helmet?',
             image: helmetLogo,
+            secondaryImage: debian,
             description: 'This is the description for the first blog post.',
         },
         {
             id: 2,
             title: 'Bookworm-slim',
             image: debian,
+            secondaryImage: debian,
             description: 'Why migrate to bookworm slim.',
         },
         {
             id: 3,
             title: 'CI/CD infra as code',
             image: cicd,
+            secondaryImage: debian,
             description: 'Maintaining legacy systems.',
         },
     ]
+
+    const handlePostClick = (id: number) => {
+        updateClickedPost(id)
+        setTimeout(() => updateClickedPost(null), 1000)
+    }
 
     return (
         <div className="p-20">
@@ -34,15 +44,21 @@ const Blog = () => {
                     {blogPosts.map((post) => (
                         <div
                             key={post.id}
-                            className="group relative overflow-hidden rounded-lg shadow-lg"
+                            className={`group relative overflow-hidden rounded-lg shadow-lg ${clickedPost === post.id ? 'scale-105 blur-md' : ''}`}
+                            onClick={() => handlePostClick(post.id)}
                         >
                             <div className="flex h-48 items-center justify-center overflow-hidden">
                                 <img
                                     src={post.image}
                                     alt={post.title}
-                                    className="group-h h-full w-full transform object-cover duration-300 group-hover:-translate-y-full"
+                                    className="group-h z-10 h-full w-full transform object-cover duration-300 group-hover:-translate-y-full"
                                 />
-                                <h2 className="absolute flex items-center justify-center bg-black bg-opacity-50 text-center text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+                                <img
+                                    src={post.secondaryImage}
+                                    alt={post.title}
+                                    className="group-h absolute left-0 top-0 z-0 h-full w-full scale-90 transform object-cover opacity-0 transition-opacity duration-1000 backdrop:filter group-hover:scale-100 group-hover:opacity-100"
+                                />
+                                <h2 className="text-1 absolute flex transform items-center justify-center bg-opacity-50 text-center text-neutral-900 opacity-0 transition-transform duration-300 group-hover:scale-125 group-hover:text-5xl group-hover:opacity-100">
                                     {post.title}
                                 </h2>
                             </div>
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -4,28 +4,37 @@ const Sidebar = () => {
     return (
         <nav className="flex flex-col justify-start rounded-lg bg-light-gray p-5 shadow-lg">
             <ul className="space-y-2">
-                <li className="pt-1">
+                <li className="relative pt-1">
                     <a
                         href="/"
-                        className="m-1 flex justify-center rounded p-1 transition hover:bg-green hover:text-light-gray"
+                        className="group m-1 flex justify-center rounded p-1 text-center transition hover:bg-green hover:text-light-gray"
                     >
                         <FaHome />
+                        <span className="absolute left-12 top-0 m-2 flex h-9 w-32 origin-left scale-0 flex-col justify-center rounded bg-navy p-2 text-gray-300 shadow-md transition-all duration-100 group-hover:scale-100">
+                            Home
+                        </span>
                     </a>
                 </li>
-                <li className="pt-1">
+                <li className="relative pt-1">
                     <a
                         href="about"
-                        className="m-1 flex justify-center rounded p-1 text-center transition hover:bg-green hover:text-light-gray"
+                        className="group m-1 flex justify-center rounded p-1 text-center transition hover:bg-green hover:text-light-gray"
                     >
                         <FaLaptopCode />
+                        <span className="absolute left-12 top-0 m-2 flex h-9 w-32 origin-left scale-0 flex-col justify-center rounded bg-navy p-2 text-gray-300 shadow-md transition-all duration-100 group-hover:scale-100">
+                            About Me
+                        </span>
                     </a>
                 </li>
-                <li className="pt-1">
+                <li className="relative pt-1">
                     <a
                         href="blog"
-                        className="m-1 flex justify-center rounded p-1 text-center transition hover:bg-green hover:text-light-gray"
+                        className="group m-1 flex justify-center rounded p-1 text-center transition hover:bg-green hover:text-light-gray"
                     >
                         <FaNewspaper />
+                        <span className="absolute left-12 top-0 m-2 flex h-9 w-32 origin-left scale-0 flex-col justify-center rounded bg-navy p-2 text-gray-300 shadow-md transition-all duration-100 group-hover:scale-100">
+                            Blog
+                        </span>
                     </a>
                 </li>
                 {/* <li className="pt-1">
