47ae70b2058a849b5cf388f99e099cfb6dda5eb1
tom_szpak <tszpak@brandwatch.com>
2024-12-27 00:28:26 +0000

    add dotenv package

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 export default defineConfig({
     base: '/',
     plugins: [react()],
     define: {
-        'import.meta.env.VITE_CMS_TOKEN': JSON.stringify(
-            process.env.VITE_CMS_TOKEN
-        ),
-        'import.meta.env.VITE_CMS_URL': JSON.stringify(
-            process.env.VITE_CMS_URL
-        ),
+        'import.meta.env.VITE_CMS_TOKEN': JSON.stringify(process.env.VITE_CMS_TOKEN),
+        'import.meta.env.VITE_CMS_URL': JSON.stringify(process.env.VITE_CMS_URL),
     },
     preview: {
         port: 8080,
@@ -23,4 +22,3 @@ export default defineConfig({
         origin: 'http://0.0.0.0:8080',
     },
 })
-    
